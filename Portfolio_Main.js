2ae1ecdb0e3c0e382c19d3109fe9fba279172a48
MorMordoch94 <60321987+MorMordoch94@users.noreply.github.com>
2020-04-06 12:27:24 +0300

    add_main_page_js_file
    add simple js file. when clicking the resume button, alert will appear.

---
+++ Portfolio_Main.js
@@ -0,0 +1,5 @@
+var resume_btn = document.querySelector("button"); // select resume button
+
+function alert_resume_btn() {
+	alert("The website is under construction. Resume will be added soon !");
+}
